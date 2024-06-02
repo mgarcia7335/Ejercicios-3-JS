ff6eba7b8aec0c87a6cafdeacc03f19b668203d8
mgarcia7335 <joooo.2008@yahoo.com.ar>
2024-06-02 20:27:12 -0300

    en linea 7 se corrigio la forma de concatenación. En las lineas 11 a la 14 se práctico caracteres

--- main.js
+++ main.js
@@ -4,10 +4,14 @@
 let texto = 'Soy Maria'
 let texto2 = 'Tengo 20 años'
 //***Proceso***//
-let resultado = 'texto + texto2'
+let resultado = `${texto} ${texto2}`
 //***Salida***//
 console.log(resultado)
 console.log(resultado.length)
+console.log(resultado[0])
+console.log(resultado[22])
+console.log(resultado[23])
+console.log(resultado[11])
 
 
 
