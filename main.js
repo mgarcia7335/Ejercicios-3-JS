d096406c609d278a62dded6560ef2b76d46582fc
mgarcia7335 <joooo.2008@yahoo.com.ar>
2024-06-02 12:24:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// Ejercio 3
+
+//***Entrada***//
+let texto = 'Soy Maria'
+let texto2 = 'Tengo 20 años'
+//***Proceso***//
+let resultado = 'texto + texto2'
+//***Salida***//
+console.log(resultado)
+console.log(resultado.length)
+
+
+
